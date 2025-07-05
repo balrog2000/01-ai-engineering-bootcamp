dbe1dd08889b293f06ef9b7633a5f2a6aeb6d14b
Tomasz Kraus <tomasz@kra.us>
2025-07-05 15:18:29 +0200

    RAG completed

--- src/chatbot-ui/core/config.py
+++ src/chatbot-ui/core/config.py
@@ -4,7 +4,18 @@ class Config(BaseSettings):
     OPENAI_API_KEY: str
     GOOGLE_API_KEY: str
     GROQ_API_KEY: str
+    QDRANT_HOST: str
+    QDRANT_COLLECTION_NAME: str
+    EMBEDDING_MODEL: str
+    EMBEDDING_MODEL_PROVIDER: str
+    LANGSMITH_PROJECT: str
+    LANGSMITH_TRACING: bool
+    LANGSMITH_ENDPOINT: str
+    LANGSMITH_API_KEY: str
+    GENERATION_MODEL: str
+    GENERATION_MODEL_PROVIDER: str
 
+    # changing because we now use docker compose env
     model_config = SettingsConfigDict(env_file=".env")
 
 config = Config()
