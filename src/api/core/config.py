d798384773665e7ee0f3e3a08777d9c23d28613e
Tomasz Kraus <tomasz@kra.us>
2025-08-21 18:43:05 +0200

    defaults

--- src/api/core/config.py
+++ src/api/core/config.py
@@ -1,29 +1,29 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Config(BaseSettings):
-    OPENAI_API_KEY: str
-    GOOGLE_API_KEY: str
-    GROQ_API_KEY: str
-    QDRANT_HOST: str
+    OPENAI_API_KEY: str = ''
+    GOOGLE_API_KEY: str = ''
+    GROQ_API_KEY: str = ''
+    QDRANT_HOST: str = ''
     QDRANT_COLLECTION_NAME_TEXT_EMBEDDINGS: str = "Amazon-items-collection-12-items"
-    QDRANT_COLLECTION_NAME_IMAGE_EMBEDDINGS: str
+    QDRANT_COLLECTION_NAME_IMAGE_EMBEDDINGS: str = ''
     QDRANT_COLLECTION_NAME_REVIEWS: str = "Amazon-items-collection-12-reviews"
-    EMBEDDING_MODEL: str
-    EMBEDDING_MODEL_PROVIDER: str
-    LANGSMITH_PROJECT: str
-    LANGSMITH_TRACING: bool
-    LANGSMITH_ENDPOINT: str
-    LANGSMITH_API_KEY: str
-    GENERATION_MODEL: str
-    GENERATION_MODEL_PROVIDER: str
+    EMBEDDING_MODEL: str = ''
+    EMBEDDING_MODEL_PROVIDER: str = ''
+    LANGSMITH_PROJECT: str = ''
+    LANGSMITH_TRACING: bool = True
+    LANGSMITH_ENDPOINT: str = ''
+    LANGSMITH_API_KEY: str = ''
+    GENERATION_MODEL: str = ''
+    GENERATION_MODEL_PROVIDER: str = ''
     PROMPT_TEMPLATE_PATH: str = "src/api/rag/prompts/rag_generation.yaml"
     PROMPT_TEMPLATE_PATH_COORDINATOR: str = "src/api/rag/prompts/coordinator_agent.yaml"
     PROMPT_TEMPLATE_PATH_PRODUCT_QA: str = "src/api/rag/prompts/product_qa_agent.yaml"
     PROMPT_TEMPLATE_PATH_SHOPPING_CART: str = "src/api/rag/prompts/shopping_cart_agent.yaml"
-    KAFKA_HOST: str
-    KAFKA_PORT: int
-    KAFKA_TOPIC: str
-    KAFKA_ENABLED: bool 
+    KAFKA_HOST: str = ''
+    KAFKA_PORT: int = 9092
+    KAFKA_TOPIC: str = ''
+    KAFKA_ENABLED: bool = False
 
     # changing because we now use docker compose env
 
